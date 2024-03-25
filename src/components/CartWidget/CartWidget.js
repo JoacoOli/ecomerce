d61602d7e272ea5b5cccbcb3d7eb827c7bc48ec4
JoacoOli <joaquinoliverosgi@gmail.com>
2024-03-25 15:01:16 -0300

    correccion_preentrega

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,9 @@
+import cart from './assets/carrito.svg'
+const CartWidget = () => {
+    return (
+        <div>
+            <img scr={cart} alt="cart-widget"/>
+        </div>
+    )
+}
+export default CartWidget
