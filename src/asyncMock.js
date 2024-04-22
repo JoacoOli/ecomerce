3f2c5f449f0aa595e628bf09338ab080078a9612
JoacoOli <joaquinoliverosgi@gmail.com>
2024-04-22 17:17:26 -0300

    falta primer video el final

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -1,11 +1,11 @@
 const products = [
     {
         id: '1',
-        name: '',
-        price: '',
-        category: '',
-        img: '',
-        stock: '',
+        name: 'Air Jordan 1 Retro',
+        price: '250',
+        category: 'nike',
+        img: 'https://th.bing.com/th/id/OIP.YdPRYTHLB6QtlWQokbLiQgHaFS?rs=1&pid=ImgDetMain',
+        stock: '13',
         description: '',
     }
 ]
